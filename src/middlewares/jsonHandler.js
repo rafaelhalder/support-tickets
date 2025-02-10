19c113ed3727a41b84ea9588eb5c07c797d163c5
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-10 18:11:04 -0300

    first commit

---
+++ src/middlewares/jsonHandler.js
@@ -0,0 +1,15 @@
+export async function jsonHandler(request, response) {
+  const buffers = [];
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+    console.log(request.body);
+  } catch (error) {
+    request.body = null;
+  }
+
+  response.setHeader("Content-type", "application/json;");
+}
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import http from "node:http";
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+async function listener(request, response) {
+  await jsonHandler(request, response);
+}
+
+http.createServer(listener).listen(3333);
