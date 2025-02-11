5d34c91f94672092eec95d5dd02a62ae599b9ac2
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-11 13:51:45 -0300

    up

---
+++ src/middlewares/routeHandler.js
@@ -0,0 +1,16 @@
+import { Database } from "../database/database.js";
+import { routes } from "../routes/index.js";
+
+const database = new Database();
+
+export function routeHandler(request, response) {
+  const route = routes.find((route) => {
+    return route.method === request.method && route.path.test(request.url);
+  });
+
+  if (route) {
+    return route.controller({ request, response, database });
+  }
+
+  return response.writeHead(404).end();
+}
